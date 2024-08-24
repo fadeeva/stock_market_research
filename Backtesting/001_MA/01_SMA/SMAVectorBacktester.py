8b1d04efd77e8c05607f26b374530135023ecedf
Katya Fadeeva <katfadmail@gmail.com>
2024-08-24 20:48:25 +0200

    bt: sma

--- Backtesting/001_MA/01_SMA/SMAVectorBacktester.py
+++ Backtesting/001_MA/01_SMA/SMAVectorBacktester.py
@@ -8,22 +8,26 @@ from scipy.optimize import brute
 
 
 class SMAVectorBacktester():
-    def __init__(self, symbol, SMA1, SMA2, start, end):
+    def __init__(self, symbol, start, end, interval, SMA1, SMA2):
         self.symbol = symbol
-        self.SMA1 = SMA1
-        self.SMA2 = SMA2
         self.start = start
         self.end = end
+        self.interval = interval
+        
+        self.SMA1 = SMA1
+        self.SMA2 = SMA2
+        
         self.results = None
         
         self.get_data()
     
     def get_data(self):
-        data = yf.download(self.symbol, start=self.start, end=self.end, interval='1d', auto_adjust=True)
+#         data = yf.download(self.symbol, start=self.start, end=self.end, interval=self.interval, auto_adjust=True)
+        data = pd.read_csv('../../../data/IBM_2002-01-01_2022-12-31_1d.csv', index_col=0, parse_dates=True)
         data = data['Close'].to_frame()
         data.rename(columns={'Close': 'price'}, inplace=True)
         
-        data['return'] = np.log(data['return'] / data['return'].shift(1))
+        data['return'] = np.log(data['price'] / data['price'].shift(1))
         data['SMA1'] = data['price'].rolling(self.SMA1).mean()
         data['SMA2'] = data['price'].rolling(self.SMA2).mean()
         
