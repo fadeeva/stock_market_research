b6f5e7c03a59edf175443a5779eb51d32a03e50c
Katya Fadeeva <katfadmail@gmail.com>
2024-08-26 20:54:22 +0200

    bt sma

--- Backtesting/001_MA/01_SMA/SMAVectorBacktester.py
+++ Backtesting/001_MA/01_SMA/SMAVectorBacktester.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import pandas as pd
 
@@ -7,12 +9,16 @@ import datetime as dt
 from scipy.optimize import brute
 
 
+DATA_STORAGE = '../../../data'
+
 class SMAVectorBacktester():
-    def __init__(self, symbol, start, end, interval, SMA1, SMA2):
-        self.symbol = symbol
-        self.start = start
-        self.end = end
-        self.interval = interval
+    def __init__(self, SMA1, SMA2, data_info):
+#         self.symbol = symbol
+#         self.start = start
+#         self.end = end
+#         self.interval = interval
+        self.data_info = data_info
+        self.data = None
         
         self.SMA1 = SMA1
         self.SMA2 = SMA2
@@ -22,19 +28,39 @@ class SMAVectorBacktester():
         self.get_data()
     
     def get_data(self):
-#         data = yf.download(self.symbol, start=self.start, end=self.end, interval=self.interval, auto_adjust=True)
-        data = pd.read_csv('../../../data/IBM_2002-01-01_2022-12-31_1d.csv', index_col=0, parse_dates=True)
-        data = data['Close'].to_frame()
-        data.rename(columns={'Close': 'price'}, inplace=True)
+        self.data_prepare()
+
+        self.data = self.data['Close'].to_frame()
+        self.data.rename(columns={'Close': 'price'}, inplace=True)
         
-        data['return'] = np.log(data['price'] / data['price'].shift(1))
-        data['SMA1'] = data['price'].rolling(self.SMA1).mean()
-        data['SMA2'] = data['price'].rolling(self.SMA2).mean()
+        self.data['return'] = np.log(data['price'] / data['price'].shift(1))
+    
+    def get_data_from_yf(self):
+        self.data = yf.download(
+            data['symbol'],
+            start=data['start'],
+            end=data['end'],
+            interval=data['interval'],
+            
+            auto_adjust=True,
+            progress=False,
+            show_errors=True
+        )
+    
+    def get_data_from_csv(self):
+        f = f'{DATA_STORAGE}/{self.data_info}'
+        if os.path.isfile(f):
+            self.data = pd.read_csv(f'{f}', index_col=0, parse_dates=True)
         
-        self.data = data
+    def data_prepare(self):
+        if type(self.data_info) is str and self.data_info[-3:]=='csv':
+            self.get_data_from_csv()
     
     def run_strategy(self):
         data = self.data.copy().dropna()
+        
+        data['SMA1'] = data['price'].rolling(self.SMA1).mean()
+        data['SMA2'] = data['price'].rolling(self.SMA2).mean()
         
         data['position'] = np.where(data['SMA1'] > data['SMA2'], 1, -1)
         data['strategy'] = data['position'].shift(1) * data['return']
